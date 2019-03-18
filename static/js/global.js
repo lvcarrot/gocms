4a85eaa0d73a0ea9d610e8cffe8fb9a92523418d
dragonflylee <dragonflylee@outlook.com>
2019-03-18 22:38:15 +0800

    js bugfix

--- static/js/global.js
+++ static/js/global.js
@@ -1,33 +1,33 @@
 if (jQuery().validate) {
   // 表单验证提示中文化
   $.extend($.validator.messages, {
-    required: "该项不能为空",
-    remote: "请修正此字段",
-    email: "请输入有效的电子邮件地址",
-    url: "请输入有效的网址",
-    date: "请输入有效的日期",
-    dateISO: "请输入有效的日期 (YYYY-MM-DD)",
-    number: "请输入有效的数字",
-    digits: "只能输入数字",
-    creditcard: "请输入有效的信用卡号码",
-    equalTo: "你的输入不相同",
-    extension: "请输入有效的后缀",
-    maxlength: $.validator.format("最多可以输入 {0} 个字符"),
-    minlength: $.validator.format("最少要输入 {0} 个字符"),
-    rangelength: $.validator.format("请输入长度在 {0} 到 {1} 之间的字符串"),
-    range: $.validator.format("请输入范围在 {0} 到 {1} 之间的数值"),
-    max: $.validator.format("请输入不大于 {0} 的数值"),
-    min: $.validator.format("请输入不小于 {0} 的数值")
+    required: '该项不能为空',
+    remote: '请修正此字段',
+    email: '请输入有效的电子邮件地址',
+    url: '请输入有效的网址',
+    date: '请输入有效的日期',
+    dateISO: '请输入有效的日期 (YYYY-MM-DD)',
+    number: '请输入有效的数字',
+    digits: '只能输入数字',
+    creditcard: '请输入有效的信用卡号码',
+    equalTo: '你的输入不相同',
+    extension: '请输入有效的后缀',
+    maxlength: $.validator.format('最多可以输入 {0} 个字符'),
+    minlength: $.validator.format('最少要输入 {0} 个字符'),
+    rangelength: $.validator.format('请输入长度在 {0} 到 {1} 之间的字符串'),
+    range: $.validator.format('请输入范围在 {0} 到 {1} 之间的数值'),
+    max: $.validator.format('请输入不大于 {0} 的数值'),
+    min: $.validator.format('请输入不小于 {0} 的数值')
   });
   // 手机号校验
-  $.validator.addMethod("mobile", function (value, element) {
+  $.validator.addMethod('mobile', function (value, element) {
     var mobile = /^((\+?86)|(\(\+86\)))?(13[0-9][0-9]{8}|15[0-9][0-9]{8}|18[0-9][0-9]{8}|17[0678][0-9]{8}|147[0-9]{8}|1349[0-9]{7})$/;
     return this.optional(element) || (value.length == 11 && mobile.test(value));
-  }, "请填写正确的手机号码");
+  }, '请填写正确的手机号码');
   // 密码验证正则表达式
-  $.validator.addMethod("regexPasswd", function (value, element) {
+  $.validator.addMethod('regexPasswd', function (value, element) {
     return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,}$/.test(value);
-  }, "密码至少包大小写字母及数字，长度至少8位");
+  }, '密码至少包大小写字母及数字，长度至少8位');
 }
 
 var Admin = {
@@ -46,13 +46,13 @@ var Admin = {
           el.remove();
         },
         errorPlacement: function (err, el) {
-          var target = el.data("target");
+          var target = el.data('target');
           if (target) {
             err.insertAfter($(target));
-          } else if (el.prop("type") === "checkbox") {
-            err.appendTo(el.closest(".checkbox-inline"));
-          } else if (el.prop("type") === "radio") {
-            err.appendTo(el.closest(".radio-inline"));
+          } else if (el.prop('type') === 'checkbox') {
+            err.appendTo(el.closest('.checkbox-inline'));
+          } else if (el.prop('type') === 'radio') {
+            err.appendTo(el.closest('.radio-inline'));
           } else {
             err.insertAfter(el);
           }
@@ -89,24 +89,24 @@ var Admin = {
   // 表单提示
   alert: function (options) {
     options = $.extend(true, {
-      container: "", // alerts parent container(by default placed after the page breadcrumbs)
-      place: "prepend", // "append" or "prepend" in container 
+      container: '', // alerts parent container(by default placed after the page breadcrumbs)
+      place: 'prepend', // "append" or "prepend" in container 
       type: 'success', // alert's type
-      message: "", // alert's message
+      message: '', // alert's message
       close: true, // make alert closable
       closeInSeconds: 0, // auto close after defined seconds
-      icon: "" // put icon before the message
+      icon: '' // put icon before the message
     }, options);
 
     var id = 'prefix_' + Math.floor(Math.random() * (new Date()).getTime());;
-    var html = '<div id="' + id + '" class="custom-alerts text-left alert alert-' + options.type + ' fade in">' + (options.close ? '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' : '') + (options.icon !== "" ? '<i class="fa-lg fa fa-' + options.icon + '"></i>  ' : '') + options.message + '</div>';
+    var html = '<div id="' + id + '" class="custom-alerts text-left alert alert-' + options.type + ' fade in">' + (options.close ? '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' : '') + (options.icon !== '' ? '<i class="fa-lg fa fa-' + options.icon + '"></i>  ' : '') + options.message + '</div>';
 
     $('.custom-alerts').remove();
 
     if (!options.container) {
       $('.content-header').after(html);
     } else {
-      if (options.place == "append") {
+      if (options.place == 'append') {
         $(options.container).append(html);
       } else {
         $(options.container).prepend(html);
@@ -146,19 +146,19 @@ var Admin = {
       // 表格复选框
       $('th :checkbox', $container).on('ifChanged', function () {
         var set = $(this).data('set');
-        var checked = $(this).is(":checked");
+        var checked = $(this).is(':checked');
         $(set).each(function () {
           if (checked) {
             $(this).iCheck('check');
-            $(this).parents('tr').addClass("active");
+            $(this).parents('tr').addClass('active');
           } else {
             $(this).iCheck('uncheck');
-            $(this).parents('tr').removeClass("active");
+            $(this).parents('tr').removeClass('active');
           }
         });
       });
       $('tr :checkbox').on('ifChanged', function () {
-        $(this).parents('tr').toggleClass("active");
+        $(this).parents('tr').toggleClass('active');
       });
     }
     if (jQuery().bootstrapSwitch) {
@@ -189,7 +189,7 @@ var Admin = {
 }
 
 // 模态框内分页
-$(document).on("click", ".modal-content a", function (e) {
+$(document).on('click', '.modal-content .pagination a,.modal-content .nav-tabs-custom a', function (e) {
   if (e.target.target == '_blank') return;
   e.preventDefault();
   $(this).closest('.modal-content').load(e.target.href, function () {
@@ -209,7 +209,7 @@ $(document).ready(function () {
     $(this).find('form').each(function (i, el) {
       Admin.validate($(el), { 'url': $(e.relatedTarget).data('href') });
     })
-  }).on("hidden.bs.modal", function () {
+  }).on('hidden.bs.modal', function () {
     $(this).find('.custom-alerts').remove();
     $(this).find('form').each(function (i, el) {
       $(el).off('.validate').removeData('validator').resetForm();
@@ -236,8 +236,8 @@ $(document).ready(function () {
           }
         }).empty();
     }
-  }).on("hide.bs.modal", function () {
-    $(this).removeData("bs.modal");
+  }).on('hide.bs.modal', function () {
+    $(this).removeData('bs.modal');
   });
   // 页面表单
   $('.login-box form, .register-box form, .box-form form').each(function (i, el) {
